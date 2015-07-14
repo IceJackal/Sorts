3c95dbaecaecb80fed695cdd795c4c338e5e5f7c
IceJackal <jack098y@gmail.com>
2015-07-14 12:53:30 +0100

    Update Insertion Sort.py

--- Insertion Sort.py
+++ Insertion Sort.py
@@ -1,10 +1,19 @@
-def InsertionSort(data):
-
-    for i in range(1, len(data)):
+def InsertionSort0(A):
+    for i in range(1, len(A)):
         j = i
-        while j > 0 and data[j-1] > data[j]:
-            hold = data[j-1]
-            data[j-1] = data[j]
-            data[j] = hold
+        while j > 0 and A[j-1] > A[j]:
+            hold = A[j-1]
+            A[j-1] = A[j]
+            A[j] = hold
             j -= 1
-    return data
+    return A
+
+def InsertionSort1(A):
+    for i in range(1, len(A)):
+        x = A[i]
+        j = i
+        while j > 0 and A[j-1] > x:
+            A[j] = A[j-1]
+            j -= 1
+        A[j] = x
+    return A
